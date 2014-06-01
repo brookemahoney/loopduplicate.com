9ce0182da1876fc0d4703e4d80de8dddb91a7e24
Jeff Mahoney <jeffy@jeffymahoney.com>
2014-06-01 16:21:57 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>LoopDuplicate</title>
+    <link type="text/css" rel="stylesheet" href="/sites/all/themes/jeffytheme/css/main.css" media="all" />
+  </head>
+  <body><a href="http://www.burningtokenrecords.com/artist/loop-duplicate"><img src="/sites/default/files/logo/loopduplicate-logo-blocks-animated-400x225.gif"></a></body>
+</html>
