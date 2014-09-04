42445cf9390e59c1c766674ca96e435f27da3314
Jeff Mahoney <jeffy@jeffymahoney.com>
2014-09-03 18:11:06 -0700

    Updates contrib modules

--- docroot/sites/all/modules/contrib/radix_layouts/plugins/layouts/radix_sutro/radix-sutro.tpl.php
+++ docroot/sites/all/modules/contrib/radix_layouts/plugins/layouts/radix_sutro/radix-sutro.tpl.php
@@ -14,7 +14,7 @@
 
   <div class="container">
     <div class="row">
-      <div class="col-md-12 panel-panel">
+      <div class="col-md-12 header panel-panel">
         <div class="panel-panel-inner">
           <?php print $content['header']; ?>
         </div>
@@ -22,12 +22,12 @@
     </div>
     
     <div class="row">
-      <div class="col-md-6 sidebar panel-panel">
+      <div class="col-md-6 column1 panel-panel">
         <div class="panel-panel-inner">
           <?php print $content['column1']; ?>
         </div>
       </div>
-      <div class="col-md-6 sidebar panel-panel">
+      <div class="col-md-6 column2 panel-panel">
         <div class="panel-panel-inner">
           <?php print $content['column2']; ?>
         </div>
@@ -35,7 +35,7 @@
     </div>
     
     <div class="row">
-      <div class="col-md-12 panel-panel">
+      <div class="col-md-12 footer panel-panel">
         <div class="panel-panel-inner">
           <?php print $content['footer']; ?>
         </div>
