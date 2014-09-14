41f68355bc0968e1113fb6e88914f338093b6a74
Jeff Mahoney <jeffy@jeffymahoney.com>
2014-09-14 15:21:06 -0700

    Removes extra comma in Gruntfile.

--- docroot/sites/all/themes/loopd_radix/Gruntfile.js
+++ docroot/sites/all/themes/loopd_radix/Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
       dist: {
         src: 'assets/javascripts/script.js',
         dest: 'assets/javascripts/script.js'
-      },
+      }
     },
     watch: {
       css: {
