3ce36b77fff950a12db568d3674d3c4ffeb50593
Jeff Mahoney <jeffy@jeffymahoney.com>
2014-09-14 11:50:45 -0700

    Updates layout of search.

---
+++ docroot/sites/all/themes/loopd_radix/plugins/layouts/loop_search/loopd-search.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Template for LoopDuplicate Search.
+ *
+ * Variables:
+ * - $css_id: An optional CSS id to use for the layout.
+ * - $content: An array of content, each item in the array is keyed to one
+ * panel of the layout. This layout supports the following sections:
+ */
+?>
+<div class="panel-display loopd-search clearfix <?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4 col-md-3 sidebar panel-panel">
+        <div class="panel-panel-inner">
+          <?php print $content['sidebar']; ?>
+        </div>
+      </div>
+      <div class="col-sm-8 col-md-9 content panel-panel">
+        <div class="panel-panel-inner">
+          <?php print $content['contentmain']; ?>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+</div>
