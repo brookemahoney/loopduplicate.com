3ae9ae640446d3f7e958971efed96cab3569755c
Jeff Mahoney <jeffy@jeffymahoney.com>
2015-01-04 08:47:31 -0800

    Updates xmlsitemap and panels everywhere.

--- docroot/sites/all/modules/contrib/panels_everywhere/theme/pane-messages.tpl.php
+++ docroot/sites/all/modules/contrib/panels_everywhere/theme/pane-messages.tpl.php
@@ -21,6 +21,6 @@
 <?php if (!empty($tabs) && (!empty($tabs['#primary']) || !empty($tabs['#secondary']))): ?>
   <div class="tabs"><?php print render($tabs); ?></div>
 <?php endif; ?>
-<?php print $action_links; ?>
-<?php print $messages; ?>
-<?php print $help; ?>
+<?php if (!empty($action_links)): ?><?php print $action_links; ?><?php endif; ?>
+<?php if (!empty($messages)): ?><?php print $messages; ?><?php endif; ?>
+<?php if (!empty($help)): ?><?php print $help; ?><?php endif; ?>
