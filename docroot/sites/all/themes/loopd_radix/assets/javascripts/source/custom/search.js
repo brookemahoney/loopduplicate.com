353feb4ebc2539b153fa01dc0d40bde9fbd94056
Jeff Mahoney <jeffy@jeffymahoney.com>
2015-02-21 11:09:30 -0800

    Fixes bug where show/hide filters ends up in the wrong state.

--- docroot/sites/all/themes/loopd_radix/assets/javascripts/source/custom/search.js
+++ docroot/sites/all/themes/loopd_radix/assets/javascripts/source/custom/search.js
@@ -5,18 +5,17 @@
 (function ($, Drupal) {
   Drupal.behaviors.loopdsearch = {
     attach: function () {
-
       var $filters = $('#search-filters');
       var $results = $('#search-results');
       if (typeof $.cookie('search-filters--visible') === 'undefined') {
         $.cookie('search-filters--visible', '1', {expires: 365, path: '/'});
       }
-      var visible = $.cookie('search-filters--visible');
+      var filters_visible = $.cookie('search-filters--visible');
 
       // On page load.
       $filters.before('<h5 id="search-toggle-filters" class="search-toggle-filters">(+/-) Show/Hide Filters</h5>');
       var $toggle = $('#search-toggle-filters');
-      if (visible === '0') {
+      if (filters_visible === '0') {
         $results.toggleClass('col-sm-8 col-md-9');
         $filters.toggleClass('hidden');
       }
@@ -25,10 +24,12 @@
       $toggle.on('click', function () {
         $results.toggleClass('col-sm-8 col-md-9');
         $filters.toggleClass('hidden');
-        if (visible === '1') {
+        if (filters_visible === '1') {
           $.cookie('search-filters--visible', '0');
+          filters_visible = '0';
         } else {
           $.cookie('search-filters--visible', '1');
+          filters_visible = '1';
         }
       });
     }
