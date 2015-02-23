b60d24f3c1688bbdb526f442f7f517525b5f634f
Jeff Mahoney <jeffy@jeffymahoney.com>
2015-02-22 16:05:19 -0800

    TravisCI: Configuration troubleshooting.

--- aliases.drushrc.php
+++ aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['site'] = array(
   'uri' => 'http://127.0.0.1:8080',
-  'root' => '/home/travis/jeffymahoney/loopduplicate.com/docroot',
+  'root' => '/home/travis/build/jeffymahoney/loopduplicate.com/docroot',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );
