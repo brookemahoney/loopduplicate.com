907b89c4505f5db1da23e8c6d7dc1fa116470286
Jeff Mahoney <jeffy@jeffymahoney.com>
2015-03-22 10:52:07 -0700

    Removes unused files from the theme.

--- docroot/sites/all/themes/loopd_radix/template.php
+++ docroot/sites/all/themes/loopd_radix/template.php
@@ -4,13 +4,9 @@
  * Theme functions
  */
 
-require_once dirname(__FILE__) . '/includes/comment.inc';
-require_once dirname(__FILE__) . '/includes/field.inc';
 require_once dirname(__FILE__) . '/includes/html.inc';
-require_once dirname(__FILE__) . '/includes/menu.inc';
 require_once dirname(__FILE__) . '/includes/node.inc';
 require_once dirname(__FILE__) . '/includes/page.inc';
-require_once dirname(__FILE__) . '/includes/search.inc';
 require_once dirname(__FILE__) . '/includes/views.inc';
 
 /**
