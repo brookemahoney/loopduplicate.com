d943df6d082bb416ed6801b40dd89afd80b62621
Jeff Mahoney <jeffy@jeffymahoney.com>
2016-06-25 16:02:32 -0700

    Updates the teaser view mode for articles and sorts the search results by the updated node date.

---
+++ docroot/sites/all/themes/loopd_radix/templates/node--article--teaser.tpl.php
@@ -0,0 +1,33 @@
+<article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page && !empty($title)): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php print $submitted; ?>
+      <?php if ($updated !== $submitted_raw): ?>
+        <small>(updated <?php print $updated; ?>)</small>
+      <?php endif; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+  <?php print render($content['links']); ?>
+
+  <?php print render($content['comments']); ?>
+
+</article>
